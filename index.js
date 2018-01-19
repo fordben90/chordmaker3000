b3976e05c8614eb11e15d7d55d80bb4c2bbef32b
Benjamin Ford <beford@beford665639.starbucks.net>
2018-01-19 12:57:45 -0800

    simplified

---
+++ index.js
@@ -0,0 +1,471 @@
+var notes = [
+  {
+    "name": "C2",
+    "class": "C2",
+    "id": 0
+  },
+  {
+    "name": "C#2",
+    "class": "C#2",
+    "id": 1
+  },
+  {
+    "name": "Db2",
+    "class": "Db2",
+    "id": 2
+  },
+  {
+    "name": "D2",
+    "class": "D2",
+    "id": 3
+  },
+  {
+    "name": "D#2",
+    "class": "D#2",
+    "id": 4
+  },
+  {
+    "name": "Eb2",
+    "class": "Eb2",
+    "id": 5
+  },
+  {
+    "name": "E2",
+    "class": "E2",
+    "id": 6
+  },
+  {
+    "name": "E#2",
+    "class": "E#2",
+    "id": 7
+  },
+  {
+    "name": "Fb2",
+    "class": "Fb2",
+    "id": 8
+  },
+  {
+    "name": "F2",
+    "class": "F2",
+    "id": 9
+  },
+  {
+    "name": "F#2",
+    "class": "F#2",
+    "id": 10
+  },
+  {
+    "name": "Gb2",
+    "class": "Gb2",
+    "id": 11
+  },
+  {
+    "name": "G2",
+    "class": "G2",
+    "id": 12
+  },
+  {
+    "name": "G#2",
+    "class": "G#2",
+    "id": 13
+  },
+   {
+    "name": "Ab2",
+    "class": "Ab2",
+    "id": 14
+  },
+  {
+    "name": "A2",
+    "class": "A2",
+    "id": 15
+  },
+  {
+    "name": "A#2",
+    "class": "A#2",
+    "id": 16
+  },
+  {
+    "name": "Bb2",
+    "class": "Bb2",
+    "id": 17
+  },
+  {
+    "name": "B2",
+    "class": "B2",
+    "id": 18
+  },
+  {
+    "name": "B#2",
+    "class": "B#2",
+    "id": 19
+  },
+  {
+    "name": "Cb2",
+    "class": "Cb2",
+    "id": 20
+  },
+  {
+    "name": "C3",
+    "class": "C3",
+    "id": 21
+  },
+  {
+    "name": "C#3",
+    "class": "C#3",
+    "id": 22
+  },
+  {
+    "name": "Db3",
+    "class": "Db3",
+    "id": 23
+  },
+  {
+    "name": "D3",
+    "class": "D3",
+    "id": 24
+  },
+  {
+    "name": "D#3",
+    "class": "D#3",
+    "id": 25
+  },
+  {
+    "name": "Eb3",
+    "class": "Eb3",
+    "id": 26
+  },
+  {
+    "name": "E3",
+    "class": "E3",
+    "id": 27
+  },
+  {
+    "name": "E#3",
+    "class": "E#3",
+    "id": 28
+  },
+  {
+    "name": "Fb3",
+    "class": "Fb3",
+    "id": 29
+  },
+  {
+    "name": "F3",
+    "class": "F3",
+    "id": 30
+  },
+  {
+    "name": "F#3",
+    "class": "F#3",
+    "id": 31
+  },
+  {
+    "name": "Gb3",
+    "class": "Gb3",
+    "id": 32
+  },
+  {
+    "name": "G3",
+    "class": "G3",
+    "id": 33
+  },
+  {
+    "name": "G#3",
+    "class": "G#3",
+    "id": 34
+  },
+   {
+    "name": "Ab3",
+    "class": "Ab3",
+    "id": 35
+  },
+  {
+    "name": "A3",
+    "class": "A3",
+    "id": 36
+  },
+  {
+    "name": "A#3",
+    "class": "A#3",
+    "id": 37
+  },
+  {
+    "name": "Bb3",
+    "class": "Bb3",
+    "id": 38
+  },
+  {
+    "name": "B3",
+    "class": "B3",
+    "id": 39
+  },
+  {
+    "name": "B#3",
+    "class": "B#3",
+    "id": 40
+  },
+  {
+    "name": "Cb3",
+    "class": "Cb3",
+    "id": 41
+  },
+  {
+    "name": "C4",
+    "class": "C4",
+    "id": 42
+  },
+  {
+    "name": "C#4",
+    "class": "C#4",
+    "id": 43
+  },
+  {
+    "name": "Db4",
+    "class": "Db4",
+    "id": 44
+  },
+  {
+    "name": "D4",
+    "class": "D4",
+    "id": 45
+  },
+  {
+    "name": "D#4",
+    "class": "D#4",
+    "id": 46
+  },
+  {
+    "name": "Eb4",
+    "class": "Eb4",
+    "id": 47
+  },
+  {
+    "name": "E4",
+    "class": "E4",
+    "id": 48
+  },
+  {
+    "name": "E#4",
+    "class": "E#4",
+    "id": 49
+  },
+  {
+    "name": "Fb4",
+    "class": "Fb4",
+    "id": 50
+  },
+  {
+    "name": "F4",
+    "class": "F4",
+    "id": 51
+  },
+  {
+    "name": "F#4",
+    "class": "F#4",
+    "id": 52
+  },
+  {
+    "name": "Gb4",
+    "class": "Gb4",
+    "id": 53
+  },
+  {
+    "name": "G4",
+    "class": "G4",
+    "id": 54
+  },
+  {
+    "name": "G#4",
+    "class": "G#4",
+    "id": 55
+  },
+   {
+    "name": "Ab4",
+    "class": "Ab4",
+    "id": 56
+  },
+  {
+    "name": "A4",
+    "class": "A4",
+    "id": 57
+  },
+  {
+    "name": "A#4",
+    "class": "A#4",
+    "id": 58
+  },
+  {
+    "name": "Bb4",
+    "class": "Bb4",
+    "id": 59
+  },
+  {
+    "name": "B4",
+    "class": "B4",
+    "id": 60
+  },
+  {
+    "name": "B#4",
+    "class": "B#4",
+    "id": 61
+  },
+  {
+    "name": "Cb4",
+    "class": "Cb4",
+    "id": 62
+  },
+  {
+    "name": "C5",
+    "class": "C5",
+    "id": 63
+  },
+  {
+    "name": "C#5",
+    "class": "C#5",
+    "id": 64
+  },
+  {
+    "name": "Db5",
+    "class": "Db5",
+    "id": 65
+  },
+  {
+    "name": "D5",
+    "class": "D5",
+    "id": 66
+  },
+  {
+    "name": "D#5",
+    "class": "D#5",
+    "id": 67
+  },
+  {
+    "name": "Eb5",
+    "class": "Eb5",
+    "id": 68
+  },
+  {
+    "name": "E5",
+    "class": "E5",
+    "id": 69
+  },
+  {
+    "name": "E#5",
+    "class": "E#5",
+    "id": 70
+  },
+  {
+    "name": "Fb5",
+    "class": "Fb5",
+    "id": 71
+  },
+  {
+    "name": "F5",
+    "class": "F5",
+    "id": 72
+  },
+  {
+    "name": "F#5",
+    "class": "F#5",
+    "id": 73
+  },
+  {
+    "name": "Gb5",
+    "class": "Gb5",
+    "id": 74
+  },
+  {
+    "name": "G5",
+    "class": "G5",
+    "id": 75
+  },
+  {
+    "name": "G#5",
+    "class": "G#5",
+    "id": 76
+  },
+   {
+    "name": "Ab5",
+    "class": "Ab5",
+    "id": 77
+  },
+  {
+    "name": "A5",
+    "class": "A5",
+    "id": 78
+  },
+  {
+    "name": "A#5",
+    "class": "A#5",
+    "id": 79
+  },
+  {
+    "name": "Bb5",
+    "class": "Bb5",
+    "id": 80
+  },
+  {
+    "name": "B5",
+    "class": "B5",
+    "id": 81
+  },
+  {
+    "name": "B#5",
+    "class": "B#5",
+    "id": 82
+  },
+  {
+    "name": "Cb5",
+    "class": "Cb5",
+    "id": 83
+  },
+  {
+    "name": "C6",
+    "class": "C6",
+    "id": 84
+  },
+
+
+];
+
+var scales = [
+  {
+    "name": "C Major",
+    "notes": [0,3,6,9,12,15,18,21]
+  },
+  {
+    "name": "G Major",
+    "notes": [12,15,18,21,3,6,10,12]
+  },
+  {
+    "name": "D Major",
+    "notes": [3,6,10,12,15,18,1,3]
+  },
+  {
+    "name": "A Major",
+    "notes": [15,18,1,3,6,10,13,15]
+  },
+  {
+    "name": "E Major",
+    "notes": [6,10,13,15,18,1,4,6]
+  },
+  {
+    "name": "B Major",
+    "notes": [18,1,4,6,10,13,16,18]
+  },
+  {
+    "name": "F Major",
+    "notes": [9,12,15,17,21,3,6,9]
+  }
+];
+
+function getScaleNotes(scale) {
+  for (i=0; i < scales.length; i++) {
+    if(scales[i].name == scale) {
+      console.log(scales[i].name);
+      // console.log(scales[i].notes);
+    }
+  }
+}
+
+getScaleNotes("D Major");
